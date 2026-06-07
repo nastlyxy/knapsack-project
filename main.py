519f1cbdcee180bbbf56aa07531e796a9635de8e
MEGAGEM <kolhan.mihail@gmail.com>
2026-06-07 11:46:08 +0200

    add brute force solution implementation in main

--- main.py
+++ main.py
@@ -15,5 +15,13 @@ def main():
     #Wczytywanie danych
     C,items = DataManager.load_data(filename)
 
-    
+    #Przekazanie danych do solvera
+    solver = Knapsack_Solver(C, items)
+
+    #Brute Force
+    print("Brute Force Solution:")
+    val_bf, chosen_bf =solver.solve_brute_force()
+    print(f"Max Value: {val_bf}")
+    print(f"Wybrane przedmioty (ID): {[item.id for item in chosen_bf]}")
+
 
